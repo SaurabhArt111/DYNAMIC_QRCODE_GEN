f95a604538b4b316223c85a4034a7eb5eac306af
SaurabhArt111 <saurabhma168@gmail.com>
2026-06-21 14:26:04 +0530

    make server.js flexible for production and developement

--- server/src/server.js
+++ server/src/server.js
@@ -14,10 +14,11 @@ import viewerRoutes from './routes/viewerRoutes.js';
 import { errorHandler, notFound } from './middleware/notFound.js';
 
 const app = express();
-
+const nodeEnv =  process.env.NODE_ENV || 'development';
+const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173';
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 app.use(compression());
-app.use(cors({ origin: env.clientUrl, credentials: true }));
+app.use(cors({ origin: `${nodeEnv === 'development' ? `http://localhost:5173` : (clientUrl || 'http://localhost:5173')}`, credentials: true }));
 app.use(express.json({ limit: '1mb' }));
 app.use(
   rateLimit({
@@ -33,7 +34,7 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/vault/:token', (req, res) => {
-  res.redirect(302, `${env.clientUrl}/vault/${req.params.token}`);
+  res.redirect(302, `${nodeEnv === 'development' ? `http://localhost:5173` : (clientUrl || 'http://localhost:5173')}/vault/${req.params.token}`);
 });
 
 app.use('/api/auth', authRoutes);
